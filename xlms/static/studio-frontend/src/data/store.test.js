a73a001e30dda82e277573eff27d6fcf2865afa5
nttlong <zugeliang2000@gamil.com>
2019-01-18 14:46:10 +0700

    save

--- xlms/static/studio-frontend/src/data/store.test.js
+++
@@ -1,8 +0,0 @@
-import store from './store';
-
-describe('Default store values', () => {
-  it('sets studioDetails properly when defined in global', () => {
-    const studioContext = {};
-    expect(store.getState().studioDetails).toEqual(studioContext);
-  });
-});
